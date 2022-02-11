cc18d1798916ed091b709e4c2b0841d663b30303
Adam Rachman <adamprmera@gmail.com>
2022-02-11 20:34:05 +0700

    jQuery Ajax and add images

--- javascripts.js
+++ javascripts.js
@@ -1,9 +1,43 @@
-document.addEventListener("DOMContentLoaded", getProducts, false);
+function number_format(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+$(document).ready(function () {
+  getProducts();
+});
 
 function getProducts() {
-  var product_list = document.querySelector("#product-list");
+  var product_list = $("#product-list");
   var html = "";
-  html += "<h5> Load This After Ready </h5>";
+  var myProducts = [];
+  $.ajax({
+    url: "products.json",
+    success: function (result) {
+      if (myProducts.length == 0) {
+        myProducts = result["products"];
+      }
+      html += '<div class="row">';
+      $.each(myProducts, function (k, v) {
+        html += '<div class="col-md-4 mb-3">';
+        html += '<div class="card">';
+        html += '<img class="card-img-top" src="products/' + v["image"] + '" alt="Card image cap">';
+        html += '<div class="card-body">';
+        html += '<h5 class="card-text">' + v["code"] + "</h5>";
+        html += '<p class="card-title">' + v["name"] + "</p>";
+        html += '<p class="card-text float-start"><small><span class="badge bg-primary">Rp. ' + number_format(v["price"]) + "</span></small></p>";
+        html += '<p class="float-end fw-bold"><small>' + number_format(v["stock"]) + " Pack</small></p>";
 
-  product_list.html(html);
+        html += '<div class="input-group">';
+        html += '<input type="text" class="form-control" id="' + v["code"] + '">';
+        html += '<button class="btn btn-outline-secondary" type="button" id="bt_' + v["code"] + '"><i class="fa-solid fa-cart-arrow-down"></i></button>';
+        html += "</div>";
+
+        html += "</div>";
+        html += "</div>";
+        html += "</div>";
+      });
+      html += "</div>";
+      product_list.html(html);
+    },
+  });
 }
